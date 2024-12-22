d458c0497874af65441753f1f6a440700e8e1631
Luis Lopez <l.lopezperdomo.e@gmail.com>
2024-12-22 13:08:07 -0300

    Fix: devolucion de errores, map modelstate

--- FinHealthAPI/Controllers/UsuarioController.cs
+++ FinHealthAPI/Controllers/UsuarioController.cs
@@ -94,9 +94,12 @@ namespace FinHealthAPI.Controllers
                     Detail = "El cuerpo de la solicitud es invalido, contiene errores de validacion.",
                     Instance = HttpContext.Request.Path,
                     Extensions = {
-                        ["errors"] = ModelState.Keys.ToDictionary(
-                            key => key,
-                            key => ModelState[key].Errors.Select(e => e.ErrorMessage).ToArray())
+                        ["errors"] = ModelState.Keys
+                            .SelectMany(key => ModelState[key].Errors.Select(error => new
+                            {
+                                Codigo = key, // Aquí puedes ajustar el código como desees
+                                Descripcion = error.ErrorMessage
+                            }))
                     }
                 });
             }
